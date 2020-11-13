359b5c5be12e021a1687b739d6291f52307c1abf
Alsmile <alsmile123@qq.com>
2020-11-13 17:06:41 +0800

    data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@ import topology from 'topology-vue';
 // 需要导入topology-vue.css
 import 'topology-vue/topology-vue.css';
 
-import { defalutMaterials, userMaterials, images } from './data';
+import { defalutMenus, defalutUserMenus, defalutMaterials, userMaterials, images } from './data';
 
 export default {
   name: 'Home',
@@ -35,54 +35,30 @@ export default {
           url: 'http://topology.le5le.com',
           target: '_blank'
         },
-        menus: [
-          {
-            name: '文件(自定义菜单1)',
-            children: [
-              { name: '新建文件', keyboard: 'Ctrl+N', action: 'open' },
-              { name: '打开文件', keyboard: 'Ctrl+O', action: 'loadNew' },
-              { name: '导入文件', keyboard: 'Ctrl+I', action: 'load' },
-              {},
-              { name: '保存', keyboard: 'Ctrl+S', action: 'save' },
-              { name: '另存为', keyboard: 'Ctrl+Shift+I', action: 'saveAs' },
-              { name: '下载JSON文件', action: 'downloadJson' },
-              { name: '下载zip打包文件', action: 'downloadZip' },
-              {},
-              { name: '导出为HTML', action: 'downloadHtml' },
-              {},
-              { name: '下载为PNG', action: 'downloadPng' },
-              { name: '下载为SVG', action: 'downloadSvg' }
-            ]
-          },
-          {
-            name: '编辑(自定义菜单2)',
-            children: [
-              { name: '撤销', keyboard: 'Ctrl+Z', action: 'undo' },
-              { name: '恢复  ', keyboard: 'Ctrl+Shift+Z', action: 'redo' },
-              {},
-              { name: '剪切  ', keyboard: 'Ctrl+X', action: 'cut' },
-              { name: '复制  ', keyboard: 'Ctrl+C', action: 'copy' },
-              { name: '粘贴  ', keyboard: 'Ctrl+V', action: 'paste' }
-            ]
-          },
-          {
-            name: '我的菜单3',
-            router: '/page'
-          }
-        ],
+        menus: defalutMenus,
         loginUrl: 'https://account.le5le.com',
         signupUrl: 'https://account.le5le.com',
-        userMenus: [
-          { name: '个人中心', router: '/user/home' },
-          {},
-          {
-            name: '账号设置',
-            url: 'https://account.le5le.com/user/profile',
-            target: '_blank'
-          },
-          {},
-          { name: '退出', action: 'logout' }
-        ]
+        userMenus: defalutUserMenus,
+        dataOptionsFn: (pen, key, value) => {
+          // pen - 当前画笔对象
+          // key - 表单输入左侧的属性名
+          // value - 仅下拉搜索时有效，当前输入文本
+          console.log('dataOptionsFn', pen, key, value);
+          // ************
+          // 根据实际业务场景 + 参数返回数组对象。
+          // Do sth.
+          // ************
+          return [{
+            label: '选项1',
+            value: 1
+          }, {
+            label: '选项2',
+            value: 2
+          }, {
+            label: '选项3',
+            value: 3
+          }];
+        }
       },
       user: {
         username: 'le5le'
@@ -98,7 +74,7 @@ export default {
         uploadParams: {
           mydata: 1
         }
-      }
+      }     
     };
   },
   methods: {
--- src/views/data.js
+++ src/views/data.js
@@ -1,3 +1,51 @@
+export const defalutMenus = [
+  {
+    name: '文件(自定义菜单1)',
+    children: [
+      { name: '新建文件', keyboard: 'Ctrl+N', action: 'open' },
+      { name: '打开文件', keyboard: 'Ctrl+O', action: 'loadNew' },
+      { name: '导入文件', keyboard: 'Ctrl+I', action: 'load' },
+      {},
+      { name: '保存', keyboard: 'Ctrl+S', action: 'save' },
+      { name: '另存为', keyboard: 'Ctrl+Shift+I', action: 'saveAs' },
+      { name: '下载JSON文件', action: 'downloadJson' },
+      { name: '下载zip打包文件', action: 'downloadZip' },
+      {},
+      { name: '导出为HTML', action: 'downloadHtml' },
+      {},
+      { name: '下载为PNG', action: 'downloadPng' },
+      { name: '下载为SVG', action: 'downloadSvg' }
+    ]
+  },
+  {
+    name: '编辑(自定义菜单2)',
+    children: [
+      { name: '撤销', keyboard: 'Ctrl+Z', action: 'undo' },
+      { name: '恢复  ', keyboard: 'Ctrl+Shift+Z', action: 'redo' },
+      {},
+      { name: '剪切  ', keyboard: 'Ctrl+X', action: 'cut' },
+      { name: '复制  ', keyboard: 'Ctrl+C', action: 'copy' },
+      { name: '粘贴  ', keyboard: 'Ctrl+V', action: 'paste' }
+    ]
+  },
+  {
+    name: '我的菜单3',
+    router: '/page'
+  }
+];
+
+export const defalutUserMenus = [
+  { name: '个人中心', router: '/user/home' },
+  {},
+  {
+    name: '账号设置',
+    url: 'https://account.le5le.com/user/profile',
+    target: '_blank'
+  },
+  {},
+  { name: '退出', action: 'logout' }
+];
+
 export const defalutMaterials = [
   {
     name: '基本形状',
